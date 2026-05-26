9aea3654ed25e8b13dab2202e56bea7a8c0d68d1
joranslaets <joranslaets11@gmail.com>
2026-05-26 15:39:50 +0200

    Classkatten

--- Kittykat/Program.cs
+++ Kittykat/Program.cs
@@ -1,6 +1,6 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace Kittykat;
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+public class Kat
+{
+    
+}
